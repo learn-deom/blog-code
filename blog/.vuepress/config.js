5a004dbd8b5ef6a94ceae86e2341a92f11fef744
power <1410837981@qq.com>
2019-12-04 22:49:11 +0800

    初始化项目

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  title: 'power的博客',
+  description: 'This is a blog example built by VuePress',
+  base: '/my-blog-vuepress/',
+  theme: '@vuepress/theme-blog',
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: '博客',
+        link: '/',
+      },
+      {
+        text: '标签',
+        link: '/tag/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/learn-deom/wangyiProfession',
+        }
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+  }
+}
